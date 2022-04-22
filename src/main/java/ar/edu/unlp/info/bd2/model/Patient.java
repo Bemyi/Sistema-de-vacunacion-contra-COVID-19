afb2390f62e17047f51615e2709a344357a06728
Benjamin Freccero <benfrecce2012@gmail.com>
2022-04-22 20:49:51 -0300

    Creación de model patient, implementación de VaxServiceImpl y VaxRepository.

--- src/main/java/ar/edu/unlp/info/bd2/model/Patient.java
+++ src/main/java/ar/edu/unlp/info/bd2/model/Patient.java
@@ -19,6 +19,10 @@ public class Patient {
         this.dayOfBirth = dayOfBirth;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public Time getDayOfBirth() {
         return dayOfBirth;
     }

5f7acd15464cb6a6002a7f78e8bb7ee5b3662c3b
gerov12 <peque_boster@hotmail.com>
2022-06-21 16:08:41 -0300

    Repositories

---
+++ src/main/java/ar/edu/unlp/info/bd2/repositories/PatientRepository.java
@@ -0,0 +1,7 @@
+package ar.edu.unlp.info.bd2.repositories;
+
+import ar.edu.unlp.info.bd2.model.Patient;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PatientRepository extends CrudRepository<Patient, Long> {
+}

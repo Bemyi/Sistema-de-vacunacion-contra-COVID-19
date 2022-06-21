4060caf0e4a9a0bf77f2bed2c6449607cc0e1432
Benjamin Freccero <benfrecce2012@gmail.com>
2022-06-21 17:02:37 -0300

    testVaccinationSchedule funcionando

--- src/main/java/ar/edu/unlp/info/bd2/repositories/SupportStaffRepository.java
+++ src/main/java/ar/edu/unlp/info/bd2/repositories/SupportStaffRepository.java
@@ -4,4 +4,5 @@ import ar.edu.unlp.info.bd2.model.SupportStaff;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SupportStaffRepository extends CrudRepository<SupportStaff, Long> {
+    SupportStaff findByDni(String dni);
 }

a2577921d512bd51ecf06c9e8d0921b36f6b4110
Felipe Mosqueira <mosqueirafelipe22@gmail.com>
2022-06-21 15:50:57 -0300

    Repositories

---
+++ src/main/java/ar/edu/unlp/info/bd2/repositories/VaccineRepository.java
@@ -0,0 +1,7 @@
+package ar.edu.unlp.info.bd2.repositories;
+
+import ar.edu.unlp.info.bd2.model.Vaccine;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VaccineRepository extends CrudRepository<Vaccine, Long> {
+}

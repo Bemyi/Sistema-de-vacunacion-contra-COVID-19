dff4781f898191a22ac739d3532c7caf1aabab55
Benjamin Freccero <benfrecce2012@gmail.com>
2022-06-21 19:30:06 -0300

    getNurseNotShot funcionando

--- src/main/java/ar/edu/unlp/info/bd2/repositories/NurseRepository.java
+++ src/main/java/ar/edu/unlp/info/bd2/repositories/NurseRepository.java
@@ -2,9 +2,15 @@ package ar.edu.unlp.info.bd2.repositories;
 
 import ar.edu.unlp.info.bd2.model.Nurse;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface NurseRepository extends CrudRepository<Nurse, Long> {
     List<Nurse> findByExperienceGreaterThan(int years);
+
+    @Query("select n " +
+                  "from Nurse as n " +
+                  "where n.id not in(select s.nurse.id from Shot as s)")
+    List<Nurse> getNurseNotShot();
 }

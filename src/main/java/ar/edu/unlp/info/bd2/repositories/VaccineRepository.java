cd0ad7be6bee9e9d36c3588412e43b92447af265
Benjamin Freccero <benfrecce2012@gmail.com>
2022-06-21 20:35:30 -0300

    GetUnappliedVaccines funcionando

--- src/main/java/ar/edu/unlp/info/bd2/repositories/VaccineRepository.java
+++ src/main/java/ar/edu/unlp/info/bd2/repositories/VaccineRepository.java
@@ -2,7 +2,14 @@ package ar.edu.unlp.info.bd2.repositories;
 
 import ar.edu.unlp.info.bd2.model.Vaccine;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface VaccineRepository extends CrudRepository<Vaccine, Long> {
     Vaccine findByName(String name);
+    @Query("select v " +
+                  "from Vaccine as v " +
+                  "where v.id not in(select s.vaccine.id from Shot as s)")
+    List<Vaccine> getUnappliedVaccines();
 }
